f28d296cb5f9b8e798bd7f9062bde71f4eb1e862
Baozoulolw <Baozoulolw@Gmail.com>
2021-06-09 09:52:45 +0800

    webchat项目成立

---
+++ src/test/java/top/baozoulolw/webchat/WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package top.baozoulolw.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
